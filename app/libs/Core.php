356b4a1809286ecd71be7cf8cd16c4982473ec52
Mary Talvistu <marytalvistu@gmail.com>
2021-01-14 01:19:22 +0200

    Kontroller on määratud ja võetud kasutusele kontrelleri objekti näol

--- app/libs/Core.php
+++ app/libs/Core.php
@@ -3,12 +3,25 @@
 
 class Core
 { // class begin
+    protected $currentController = 'Pages';
+    protected $currentMethod = 'index';
+    protected $params = [];
     // constructor
 
     // get url data
     public function __construct()
     {
-        $this->geturl();
+        $url = $this->geturl();
+        $controllerName = ucwords($url[0]);
+        $controllerFile = '../app/controllers/'.$controllerName.'php';
+        if(file_exists($controllerFile)) {
+            $this->currentController = $controllerName;
+            unset($url[0]);
+        }
+        require_once '../app/controllers/'.$this->currentController.'.php';
+       $this->currentController = new $this->currentController;
+        print_r($this->currentController);
+        print_r($url);
     }
 
     public function geturl(){
