28f3b046b183c6c4aae54193fe5be28a3ba98ac2
Mary Talvistu <marytalvistu@gmail.com>
2021-01-20 00:45:41 +0200

    Loodud ja testitud ühendus andmebaasiga

--- app/config/constants.php
+++ app/config/constants.php
@@ -5,3 +5,10 @@ define('PROJECT_NAME', 'pvk');
 define('APPROOT', dirname(dirname('___FILE___')));
 // URL root
 define('URLROOT', 'http://'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
+
+
+// db params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'pvk');
