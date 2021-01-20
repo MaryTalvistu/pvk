3410243ef28ac0811a3b7fb12075620bfc53e8e9
Mary Talvistu <marytalvistu@gmail.com>
2021-01-20 12:23:00 +0200

    Rakenduse põhi - raamistik on valmis

--- README.md
+++ README.md
@@ -1,12 +1,3 @@
 # pvk
-***
-This is a test project for learning about MVC (Model View Controller) framework. 
 
-### Installing
-```
-$ git clone https://example.com
-$ cd ../path/to/the/file
-$ npm install
-$ npm start
-```
 
--- app/config/constants.php
+++ app/config/constants.php
@@ -2,7 +2,7 @@
  // Project name
 define('PROJECT_NAME', 'projectname');
 // App root dir
-define('APPROOT', dirname(dirname('___FILE___')));
+define('APPROOT', dirname(dirname(__FILE__)));
 // URL root
 define('URLROOT', 'http://'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
 
